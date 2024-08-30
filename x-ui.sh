ed433f170fb0b3093e74b1eae6be8534d1988412
admin6501 <154446051+admin6501@users.noreply.github.com>
2024-08-30 14:40:30 +0330

    Update x-ui.sh

--- x-ui.sh
+++ x-ui.sh
@@ -1,34 +1,36 @@
 #!/bin/bash
 
-# Function to add or subtract volume for all users
+# Function to add volume for all users
 modify_volume_all_users() {
-    read -p "Enter the volume (in MB) (use negative sign to subtract): " volume
+    read -p "Enter the volume (in GB) (use negative sign to subtract): " volume_gb
+    # Convert GB to MB
+    volume_mb=$(echo "$volume_gb * 1024" | bc)
     # Get the list of users
     users=$(x-ui list-users | awk '{print $1}')
     for username in $users; do
-        # Command to add or subtract volume for the user
-        x-ui update --user $username --volume $volume
-        echo "Volume of $volume MB has been modified for user $username."
+        # Command to add volume for the user without updating the panel
+        result=$(x-ui update --user $username --volume $volume_mb --no-update)
+        echo "Volume of $volume_gb GB ($volume_mb MB) has been added for user $username. Result: $result"
     done
 }
 
-# Function to add or subtract time for all users
+# Function to add time for all users
 modify_time_all_users() {
     read -p "Enter the time (in days) (use negative sign to subtract): " days
     # Get the list of users
     users=$(x-ui list-users | awk '{print $1}')
     for username in $users; do
-        # Command to add or subtract time for the user
-        x-ui update --user $username --days $days
-        echo "Time of $days days has been modified for user $username."
+        # Command to add time for the user without updating the panel
+        result=$(x-ui update --user $username --days $days --no-update)
+        echo "Time of $days days has been added for user $username. Result: $result"
     done
 }
 
 # Main menu
 while true; do
     echo "Main Menu:"
-    echo "1. Add/Subtract Volume for All Users"
-    echo "2. Add/Subtract Time for All Users"
+    echo "1. Add Volume for All Users"
+    echo "2. Add Time for All Users"
     echo "3. Exit"
     read -p "Please choose an option: " choice
 
